db9a492bcf332ed605635c599d4f179f7507683d
unknown <letitslide87@gmail.com>
2018-04-27 17:13:56 -0400

    Added responsive css layout, removed some initial css settings.

--- js/main.js
+++ js/main.js
@@ -137,29 +137,30 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
-
+  
+  const name = document.createElement('h1');
+  name.innerHTML = restaurant.name;
+  li.append(name);
+  
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
-
-  const name = document.createElement('h1');
-  name.innerHTML = restaurant.name;
-  li.append(name);
-
-  const neighborhood = document.createElement('p');
+  
+  const neighborhood = document.createElement('h2');
   neighborhood.innerHTML = restaurant.neighborhood;
   li.append(neighborhood);
-
+  
+  
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
   li.append(address);
-
+  
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
-
+  
   return li
 }
 
